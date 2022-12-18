787c7d2794150a1428a0af965b1c022c2220bcaf
h ran <hran@eichinoMacBook-Air.local>
2022-12-18 10:17:43 -0800

    initial commit

---
+++ main/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm 
+from .models import Talk, User
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email",)
+
+class LoginForm(AuthenticationForm):
+    pass
+
+class TalkForm(forms.ModelForm):
+    class Meta:
+        model = Talk
+        fields = ("message",)
+
+# 以下を追加
+class UsernameChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+        labels = {"username": "新しいユーザー名"}
+        help_texts = {"username": ""}
+
+class EmailChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("email",)
+        labels = {"email": "新しいメールアドレス"}
+
