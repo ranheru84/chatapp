26251985705bcf22d117588994871d64384f1125
h ran <hran@eichinoMacBook-Air.local>
2023-12-29 14:08:56 +0900

    設定ファイルの分割

---
+++ chat_app/settings/dev.py
@@ -0,0 +1,5 @@
+from .base import *
+
+ALLOWED_HOSTS = ['*']
+
+INTERNAL_IPS = ['127.0.0.1']
--- manage.py
+++ manage.py
@@ -5,8 +5,12 @@ import sys
 
 
 def main():
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
